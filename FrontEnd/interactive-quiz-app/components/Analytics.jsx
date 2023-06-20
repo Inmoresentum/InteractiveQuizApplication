bb112a70dc1d297ea7995286fd7e940697a3616e
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-06-20 22:22:28 +0600

    :bug: fix: fixed the bug where images won't be displayed properly.
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- FrontEnd/interactive-quiz-app/components/Analytics.jsx
+++ FrontEnd/interactive-quiz-app/components/Analytics.jsx
@@ -1,10 +1,11 @@
 import Laptop from "../public/laptop.jpg"
+import Image from "next/image";
 
 const Analytics = () => {
     return (
         <div className='w-full bg-white py-16 px-4 wavy'>
             <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
-                <img className='w-[500px] mx-auto my-4 rounded-full' src={Laptop} alt='/'/>
+                <Image className='w-[500px] mx-auto my-4 rounded-full' src={Laptop} alt='/'/>
                 <div className='flex flex-col justify-center'>
                     <p className='text-purple-600 font-bold '>DATA ANALYTICS DASHBOARD</p>
                     <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Manage Data Analytics Centrally</h1>
