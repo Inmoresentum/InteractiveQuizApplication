d420cb4a5d28c2c174fbeb19b0c7da6326f1800a
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-06-24 21:46:31 +0600

    docs: updated OpenApiConfig
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- src/main/java/com/quiz_app/config/OpenApiConfig.java
+++ src/main/java/com/quiz_app/config/OpenApiConfig.java
@@ -33,7 +33,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
                 ),
                 @Server(
                         description = "PROD ENV",
-                        url = "https://to-be-added.tech"
+                        url = "https://to-be-added.tec"
                 )
         },
         security = {
