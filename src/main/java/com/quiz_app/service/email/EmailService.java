5d2c5d43c7ad0e0dd37b887f7b72bd037ea44d97
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-06-24 21:30:55 +0600

    :sparkles: feat: added the ability to send email.
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

---
+++ src/main/java/com/quiz_app/service/email/EmailService.java
@@ -0,0 +1,40 @@
+package com.quiz_app.service.email;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class EmailService implements EmailSender {
+    private final static Logger LOGGER = LoggerFactory
+            .getLogger(EmailService.class);
+
+    private final JavaMailSender mailSender;
+
+    @Override
+    @Async
+    //Todo: Later implement rabbitmq just like a large scale
+    // production application.
+    public void send(String to, String subject, String email) {
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+            helper.setText(email, true);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            String from = "service@interactive-quiz-app.com";
+            helper.setFrom(from);
+            mailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            LOGGER.error("failed to send email", e);
+            throw new IllegalStateException("Failed to send the EMAIL");
+        }
+    }
+}
