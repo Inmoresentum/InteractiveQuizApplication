434aa905e3a8f3af17f3c1ac9ab24c03775f2f3b
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-06-28 19:23:50 +0600

    fix: got rid global body selector and scoped styles to their own page for now
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- FrontEnd/interactive-quiz-app/app/page.js
+++ FrontEnd/interactive-quiz-app/app/page.js
@@ -3,12 +3,12 @@ import Hero from "@/components/landingPage/Hero";
 import Newsletter from "@/components/landingPage/NewsLetter";
 import Cards from "@/components/landingPage/Cards";
 import Testimonial from "@/components/landingPage/Testimonial";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import Navbar from "@/components/landingPage/Navbar";
+import Footer from "@/components/landingPage/Footer";
 
 export default function Home() {
     return (
-        <div>
+        <div className="bg-[#000300]">
             <Navbar/>
             <Hero/>
             <Analytics/>
