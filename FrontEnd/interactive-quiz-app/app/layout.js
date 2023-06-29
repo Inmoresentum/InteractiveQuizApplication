b9fe78fec0e4fcf9550965c70120c5fcec77520d
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-06-30 02:13:20 +0600

    feat: added a CustomCursor.jsx which creates a nice looking rounded custom cursor for the lading page only.
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- FrontEnd/interactive-quiz-app/app/layout.js
+++ FrontEnd/interactive-quiz-app/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import CustomCursor from "@/components/customCursor/CustomCursor";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+      <CustomCursor/>
       {children}
       </body>
     </html>
