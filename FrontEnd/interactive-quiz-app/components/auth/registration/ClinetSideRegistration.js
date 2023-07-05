b827da2b35a642a579c5629b9ed013d76d78977d
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-07-05 21:56:00 +0600

    chore: added a bit more styles
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- FrontEnd/interactive-quiz-app/components/auth/registration/ClinetSideRegistration.js
+++ FrontEnd/interactive-quiz-app/components/auth/registration/ClinetSideRegistration.js
@@ -1,6 +1,11 @@
 "use client"
 import {useRef, useState} from 'react';
-import {RiLockPasswordLine, RiMailLine, RiUserLine} from "react-icons/ri";
+import {
+    RiLockPasswordLine,
+    RiMailLine,
+    RiPhoneLine,
+    RiUserAddLine
+} from "react-icons/ri";
 import {motion} from "framer-motion";
 import Link from "next/link";
 import Image from "next/image";
@@ -11,7 +16,19 @@ export default function ClientSideRegiFrom() {
     const dragAbleConstraints = useRef(null);
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [countryCode, setCountryCode] = useState('+880');
+    const [phoneNumber, setPhoneNumber] = useState('');
 
+    const handleCountryCodeChange = (e) => {
+        setCountryCode(e.target.value);
+    };
+
+    const handlePhoneNumberChange = (e) => {
+        const input = e.target.value;
+        // Remove any non-numeric characters from the input
+        const sanitizedInput = input.replace(/\D/g, '');
+        setPhoneNumber(sanitizedInput);
+    };
     const handleSubmit = (e) => {
         e.preventDefault();
         // handle form submission
@@ -56,12 +73,13 @@ export default function ClientSideRegiFrom() {
                             </label>
                             <div className="relative">
                 <span className="absolute left-3 top-3 text-gray-600">
-                  <RiMailLine size={24}/>
+                  {/*<RiMailLine size={24}/>*/}
+                    <RiUserAddLine size={24}/>
                 </span>
                                 <input
                                     type="text"
                                     id="username"
-                                    className="w-full pl-10 px-6 py-3 bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-300 focus:outline-none focus:border-blue-500 rounded-3xl text-gray-800 text-base shadow-md"
+                                    className="w-full pl-10 px-6 py-3 bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-900 focus:outline-none focus:border-blue-500 rounded-3xl text-gray-800 text-base shadow-md"
                                     // onChange={(e)=> handleChangeEmail(e)}
                                     placeholder="Enter Your Username"
                                     required={true}
@@ -83,7 +101,7 @@ export default function ClientSideRegiFrom() {
                                 <input
                                     type="email"
                                     id="email"
-                                    className="w-full pl-10 px-6 py-3 bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-300 focus:outline-none focus:border-blue-500 rounded-3xl text-gray-800 text-base shadow-md"
+                                    className="w-full pl-10 px-6 py-3 bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-900 focus:outline-none focus:border-blue-500 rounded-3xl text-gray-800 text-base shadow-md"
                                     // onChange={(e)=> handleChangeEmail(e)}
                                     placeholder="Enter Your email"
                                     required={true}
@@ -105,7 +123,7 @@ export default function ClientSideRegiFrom() {
                                 <input
                                     type="password"
                                     id="password"
-                                    className="w-full pl-10 px-6 py-3 bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-300 focus:outline-none focus:border-blue-500 rounded-3xl text-gray-800 text-base shadow-md"
+                                    className="w-full pl-10 px-6 py-3 bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-900 focus:outline-none focus:border-blue-500 rounded-3xl text-gray-800 text-base shadow-md"
                                     // onChange={handleChangePassword}
                                     placeholder="Enter Your password"
                                 />
@@ -126,9 +144,9 @@ export default function ClientSideRegiFrom() {
                                 <input
                                     type="password"
                                     id="confirm-password"
-                                    className="w-full pl-10 px-6 py-3 bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-300 focus:outline-none focus:border-blue-500 rounded-3xl text-gray-800 text-base shadow-md"
+                                    className="w-full pl-10 px-6 py-3 bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-900 focus:outline-none focus:border-blue-500 rounded-3xl text-gray-800 text-base shadow-md"
                                     // onChange={handleChangePassword}
-                                    placeholder="Enter Your password"
+                                    placeholder="Enter your password again"
                                 />
 
                             </div>
@@ -144,6 +162,67 @@ export default function ClientSideRegiFrom() {
                         {/*        Forgot password?*/}
                         {/*    </Link>*/}
                         {/*</div>*/}
+                        <div className="relative mb-6">
+                            <label htmlFor="phone" className="text-lg px-4 font-medium mb-2 text-gray-800">
+                                Phone Number
+                            </label>
+                            <div className="relative flex">
+                                <select
+                                    value={countryCode}
+                                    onChange={handleCountryCodeChange}
+                                    className="h-12 bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-900 focus:outline-none focus:border-blue-500 rounded-3xl text-gray-800 text-base shadow-md mr-0.5 mb-2"
+                                >
+                                    <option
+                                        className="bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-900 focus:outline-none focus:border-blue-500 rounded-3xl text-gray-800 text-base shadow-md"
+                                        value="+880">+880
+                                    </option>
+                                    <option
+                                        className="bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-900 focus:outline-none focus:border-blue-500 rounded-3xl text-gray-800 text-base shadow-md"
+                                        value="+1">+1
+                                    </option>
+                                    <option
+                                        className="bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-900 focus:outline-none focus:border-blue-500 rounded-3xl text-gray-800 text-base shadow-md"
+                                        value="+44">+44
+                                    </option>
+                                </select>
+                                <div className="relative">
+                                    <span className="absolute left-3 top-3 text-gray-600">
+                                          <RiPhoneLine size={24}/>
+                                    </span>
+                                    <input
+                                        type="tel"
+                                        id="phone"
+                                        value={phoneNumber}
+                                        onChange={handlePhoneNumberChange}
+                                        className="w-[280px]  pl-10 px-6 py-3 bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-900 focus:outline-none focus:border-blue-500 rounded-3xl text-gray-800 text-base shadow-md"
+                                        placeholder="Enter your phone number"
+                                        required={true}
+                                    />
+                                </div>
+                            </div>
+
+                            <div class="flex items-center justify-center w-full">
+                                <label htmlFor="dropzone-file"
+                                       className="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-400 border-dashed rounded-lg cursor-pointer bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md hover:bg-opacity-30 dark:hover:bg-opacity-30 dark:bg-gray-700 dark:border-gray-600 dark:hover:border-gray-500">
+                                    <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                                        <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                                             aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                             viewBox="0 0 20 16">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                  stroke-width="2"
+                                                  d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2l2 2"/>
+                                        </svg>
+                                        <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span
+                                            class="font-semibold">Click to upload</span> or drag and drop</p>
+                                        <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX.
+                                            800x400px)</p>
+                                    </div>
+                                    <input id="dropzone-file" type="file" className="hidden"/>
+                                </label>
+                            </div>
+
+                        </div>
+
                         <button
                             type="button"
                             onClick={handleSubmit}
