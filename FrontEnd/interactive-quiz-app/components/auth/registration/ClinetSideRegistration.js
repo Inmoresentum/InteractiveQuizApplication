bd53c6b587f5504407d3b5bf2f65a8b4c869d35e
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-07-06 12:47:16 +0600

    fix: fixed some visual glitches and improved css animation performance and added more fields
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- FrontEnd/interactive-quiz-app/components/auth/registration/ClinetSideRegistration.js
+++ FrontEnd/interactive-quiz-app/components/auth/registration/ClinetSideRegistration.js
@@ -6,6 +6,12 @@ import {
     RiPhoneLine,
     RiUserAddLine
 } from "react-icons/ri";
+import {
+    FaDiscord,
+    FaGithubSquare,
+    FaGoogle
+}
+    from "react-icons/fa";
 import {motion} from "framer-motion";
 import Link from "next/link";
 import Image from "next/image";
@@ -55,16 +61,22 @@ export default function ClientSideRegiFrom() {
                         <Image
                             src={quizAppLogo}
                             alt={reactLogo}
-                            className="w-[110px] h-[110px] rounded-2xl hover:scale-105 hover:brightness-125 hover:contrast-150 hover:saturate-150 transition-all duration-300 ease-in-out"
+                            className="w-[110px] h-[110px] rounded-2xl hover:scale-105 hover:brightness-125
+                             hover:contrast-150 hover:saturate-150 transition-all duration-300 ease-in-out"
                         />
                     </Link>
+                    <h2 className="font-bold text-2xl mt-4">
+                        Create An Account Using
+                        {/*<FaGithubSquare className="ml-0.5 mr-0.5 cursor-pointer hover:scale-110 duration-300 ease-in" size={24}/>*/}
+                        {/*or <FaGoogle className="ml-0.5 mr-0.5 cursor-pointer hover:scale-110 duration-300 ease-in" size={24}/>*/}
+                    </h2>
+                    <div className="flex justify-evenly w-full">
+                    <FaGithubSquare className="ml-0.5 mr-0.5 text-gray-700 cursor-pointer hover:scale-110 hover:text-black duration-300 ease-in" size={44}/>
+                    <FaGoogle className="ml-0.5 mr-0.5 cursor-pointer hover:scale-110 hover:text-orange-500  duration-300 ease-in" size={44}/>
+                    <FaDiscord className="ml-0.5 mr-0.5 cursor-pointer hover:scale-110 hover:text-blue-500 duration-300 ease-in" size={44}/>
+                    </div>
+                    <h2 className="text-2xl font-bold italic mt-4 antialiased"> OR</h2>
                     <h2 className="text-2xl font-bold mb-4 mt-4 animate-pulse">REGISTRATION FORM</h2>
-                    {/*{errorMessage?*/}
-                    {/*    <div class="flex justify-center items-center">*/}
-                    {/*        <div class="bg-white rounded-xl border border-gray-300 shadow-lg p-4 max-w-sm animate-bounce backdrop-filter backdrop-blur-md bg-opacity-50 ">*/}
-                    {/*            <p class="font-bold text-red-500 text-center">{errorMessage}</p>*/}
-                    {/*        </div>*/}
-                    {/*    </div> : "" }*/}
                     <form className="max-w-md mx-auto">
 
                         <div className="relative mb-6">
@@ -79,7 +91,10 @@ export default function ClientSideRegiFrom() {
                                 <input
                                     type="text"
                                     id="username"
-                                    className="w-full pl-10 px-6 py-3 bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-900 focus:outline-none focus:border-blue-500 rounded-3xl text-gray-800 text-base shadow-md"
+                                    className="w-full pl-10 px-6 py-3 bg-opacity-20 bg-white bg-clip-padding
+                                       backdrop-filter backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-900
+                                       focus:outline-none focus:border-blue-500 rounded-3xl
+                                       text-gray-800 text-base shadow-md"
                                     // onChange={(e)=> handleChangeEmail(e)}
                                     placeholder="Enter Your Username"
                                     required={true}
@@ -101,16 +116,16 @@ export default function ClientSideRegiFrom() {
                                 <input
                                     type="email"
                                     id="email"
-                                    className="w-full pl-10 px-6 py-3 bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-900 focus:outline-none focus:border-blue-500 rounded-3xl text-gray-800 text-base shadow-md"
+                                    className="w-full pl-10 px-6 py-3 bg-opacity-20 bg-white bg-clip-padding
+                                     backdrop-filter backdrop-blur-md placeholder-gray-500
+                                      focus:placeholder-gray-900 focus:outline-none focus:border-blue-500
+                                       rounded-3xl text-gray-800 text-base shadow-md"
                                     // onChange={(e)=> handleChangeEmail(e)}
                                     placeholder="Enter Your email"
                                     required={true}
                                 />
 
                             </div>
-                            {/*{emailError && (*/}
-                            {/*    <p className="font-bold text-red-800 text-sm m-2 px-[8px]">Please enter a valid email address.</p>*/}
-                            {/*)}*/}
                         </div>
                         <div className="relative mb-6">
                             <label htmlFor="password" className="text-lg px-4 font-medium mb-2 text-gray-800">
@@ -123,15 +138,15 @@ export default function ClientSideRegiFrom() {
                                 <input
                                     type="password"
                                     id="password"
-                                    className="w-full pl-10 px-6 py-3 bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-900 focus:outline-none focus:border-blue-500 rounded-3xl text-gray-800 text-base shadow-md"
+                                    className="w-full pl-10 px-6 py-3 bg-opacity-20 bg-white bg-clip-padding
+                                     backdrop-filter backdrop-blur-md placeholder-gray-500
+                                      focus:placeholder-gray-900 focus:outline-none focus:border-blue-500
+                                       rounded-3xl text-gray-800 text-base shadow-md"
                                     // onChange={handleChangePassword}
                                     placeholder="Enter Your password"
                                 />
 
                             </div>
-                            {/*{passwordError && (*/}
-                            {/*    <p className="text-red-800 text-sm m-2 px-[8px]">Please enter a password.</p>*/}
-                            {/*)}*/}
                         </div>
                         <div className="relative mb-6">
                             <label htmlFor="password" className="text-lg px-4 font-medium mb-2 text-gray-800">
@@ -144,24 +159,16 @@ export default function ClientSideRegiFrom() {
                                 <input
                                     type="password"
                                     id="confirm-password"
-                                    className="w-full pl-10 px-6 py-3 bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-900 focus:outline-none focus:border-blue-500 rounded-3xl text-gray-800 text-base shadow-md"
+                                    className="w-full pl-10 px-6 py-3 bg-opacity-20 bg-white bg-clip-padding backdrop-filter
+                                     backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-900
+                                      focus:outline-none focus:border-blue-500 rounded-3xl text-gray-800 text-base shadow-md"
                                     // onChange={handleChangePassword}
                                     placeholder="Enter your password again"
                                 />
 
                             </div>
-                            {/*{passwordError && (*/}
-                            {/*    <p className="text-red-800 text-sm m-2 px-[8px]">Please enter a password.</p>*/}
-                            {/*)}*/}
                         </div>
-                        {/*<div className="flex justify-end items-center mb-6">*/}
-                        {/*    <Link href={"/auth/forgot/password"}*/}
-                        {/*          className="text-gray-200 hover:text-black hover:underline hover:translate-y-[-2px] transition-colors duration-300 ease-linear tooltip"*/}
-                        {/*          data-tooltip="Click Here to Reset Your Password"*/}
-                        {/*    >*/}
-                        {/*        Forgot password?*/}
-                        {/*    </Link>*/}
-                        {/*</div>*/}
+
                         <div className="relative mb-6">
                             <label htmlFor="phone" className="text-lg px-4 font-medium mb-2 text-gray-800">
                                 Phone Number
@@ -170,18 +177,27 @@ export default function ClientSideRegiFrom() {
                                 <select
                                     value={countryCode}
                                     onChange={handleCountryCodeChange}
-                                    className="h-12 bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-900 focus:outline-none focus:border-blue-500 rounded-3xl text-gray-800 text-base shadow-md mr-0.5 mb-2"
+                                    className="h-12 bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md
+                                     placeholder-gray-500 focus:placeholder-gray-900 focus:outline-none focus:border-blue-500
+                                      rounded-3xl text-gray-800 text-base shadow-md mr-0.5 mb-2"
                                 >
                                     <option
-                                        className="bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-900 focus:outline-none focus:border-blue-500 rounded-3xl text-gray-800 text-base shadow-md"
+                                        className="bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md
+                                         placeholder-gray-500 focus:placeholder-gray-900 focus:outline-none focus:border-blue-500
+                                          rounded-3xl text-gray-800 text-base shadow-md"
                                         value="+880">+880
                                     </option>
                                     <option
-                                        className="bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-900 focus:outline-none focus:border-blue-500 rounded-3xl text-gray-800 text-base shadow-md"
+                                        className="bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md
+                                         placeholder-gray-500 focus:placeholder-gray-900 focus:outline-none
+                                          focus:border-blue-500 rounded-3xl text-gray-800 text-base shadow-md"
                                         value="+1">+1
                                     </option>
                                     <option
-                                        className="bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-900 focus:outline-none focus:border-blue-500 rounded-3xl text-gray-800 text-base shadow-md"
+                                        className="bg-opacity-20 bg-white bg-clip-padding backdrop-filter
+                                         backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-900
+                                          focus:outline-none focus:border-blue-500 rounded-3xl text-gray-800
+                                           text-base shadow-md"
                                         value="+44">+44
                                     </option>
                                 </select>
@@ -194,7 +210,10 @@ export default function ClientSideRegiFrom() {
                                         id="phone"
                                         value={phoneNumber}
                                         onChange={handlePhoneNumberChange}
-                                        className="w-[280px]  pl-10 px-6 py-3 bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-900 focus:outline-none focus:border-blue-500 rounded-3xl text-gray-800 text-base shadow-md"
+                                        className="w-[280px]  pl-10 px-6 py-3 bg-opacity-20 bg-white bg-clip-padding
+                                         backdrop-filter backdrop-blur-md placeholder-gray-500 focus:placeholder-gray-900
+                                          focus:outline-none focus:border-blue-500 rounded-3xl text-gray-800
+                                           text-base shadow-md"
                                         placeholder="Enter your phone number"
                                         required={true}
                                     />
@@ -203,21 +222,26 @@ export default function ClientSideRegiFrom() {
 
                             <div class="flex items-center justify-center w-full">
                                 <label htmlFor="dropzone-file"
-                                       className="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-400 border-dashed rounded-lg cursor-pointer bg-opacity-20 bg-white bg-clip-padding backdrop-filter backdrop-blur-md hover:bg-opacity-30 dark:hover:bg-opacity-30 dark:bg-gray-700 dark:border-gray-600 dark:hover:border-gray-500">
+                                       className="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-400
+                                        border-dashed rounded-lg cursor-pointer bg-opacity-20 bg-white bg-clip-padding
+                                         backdrop-filter backdrop-blur-md hover:bg-opacity-30 dark:hover:bg-opacity-30
+                                          dark:bg-gray-700 dark:border-gray-600 dark:hover:border-gray-500">
                                     <div class="flex flex-col items-center justify-center pt-5 pb-6">
                                         <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
                                              aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                                              viewBox="0 0 20 16">
                                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                                   stroke-width="2"
-                                                  d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2l2 2"/>
+                                                  d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5
+                                                   0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0
+                                                    8h2.167M10 15V6m0 0L8 8m2-2l2 2"/>
                                         </svg>
                                         <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span
                                             class="font-semibold">Click to upload</span> or drag and drop</p>
                                         <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX.
                                             800x400px)</p>
                                     </div>
-                                    <input id="dropzone-file" type="file" className="hidden"/>
+                                    <input id="dropzone-file" type="file" className="hidden" accept="image/vnd.sealedmedia.softseal.jpg, .png, .svg"/>
                                 </label>
                             </div>
 
@@ -226,7 +250,10 @@ export default function ClientSideRegiFrom() {
                         <button
                             type="button"
                             onClick={handleSubmit}
-                            className="w-[350px] text-white py-2 px-4 rounded-full transition-colors duration-300 ease-in-out bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 hover:from-blue-400 hover:via-indigo-500 hover:to-purple-500 focus:from-blue-400 focus:via-indigo-500 focus:to-purple-500 login-button-animate-gradient-x"
+                            className="w-[350px] text-white py-2 px-4 rounded-full transition-colors duration-300
+                             ease-in-out bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 hover:from-blue-400
+                              hover:via-indigo-500 hover:to-purple-500 focus:from-blue-400 focus:via-indigo-500
+                               focus:to-purple-500 login-button-animate-gradient-x"
                         >
                             Register
                         </button>
@@ -236,7 +263,9 @@ export default function ClientSideRegiFrom() {
                             Already have an Account?
                             <Link
                                 href={"/auth/login/"}
-                                className="text-white hover:text-black hover:underline hover:translate-y-[-2px] transition-colors duration-300 ease-linear ml-2 tooltip"
+                                className="text-white hover:text-black hover:underline
+                                hover:translate-y-[-2px] transition-colors duration-300 ease-linear ml-2
+                                 tooltip"
                                 data-tooltip="Click Here to Visit Login Page">
                                 Login
                             </Link>
