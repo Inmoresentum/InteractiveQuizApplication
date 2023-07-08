c9992e5c026e967fb850ed4f4149098357d270ff
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-07-09 00:35:40 +0600

    feat: added re-usable custom quiz components
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

---
+++ FrontEnd/interactive-quiz-app/components/quiz-components/Core-Compoents/Explanation.jsx
@@ -0,0 +1,24 @@
+const Explanation = function ({ question, isResultPage }) {
+    const { explanation } = question;
+
+    if (!explanation) {
+        return null;
+    }
+
+    if (isResultPage) {
+        return (
+            <div className="explanation">
+                {explanation}
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <br />
+            {explanation}
+        </div>
+    );
+};
+
+export default Explanation;
