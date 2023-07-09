b1753a5a3715969668b717031dff05f233932616
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-07-09 20:04:35 +0600

    fix: temporarily fixed dynamic metadata generation
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- FrontEnd/interactive-quiz-app/app/quiz/public/page.jsx
+++ FrontEnd/interactive-quiz-app/app/quiz/public/page.jsx
@@ -1,10 +1,27 @@
 import QuizProvider from "@/components/quiz/quiz-wrapper/QuizProvider";
 import {quiz} from "@/TestData/Quiz";
 
+export async function generateMetadata() {
+    // fetch data
+    // const product = await fetch(`https://.../${id}`).then((res) => res.json())
+    //
+    // // optionally access and extend (rather than replace) parent metadata
+    // const previousImages = (await parent).openGraph?.images || []
+    // console.log(quiz)
+    const {quizTitle, quizSynopsis} = quiz
+    return {
+        title: quizTitle,
+        description: quizSynopsis
+    }
+}
+
 export default function DemoQuiz() {
     return (
-        <div className="flex items-center content-center">
-            <QuizProvider quiz={quiz}/>
+        <div className="flex items-center justify-center">
+            <div className="flex justify-center items-center w-[1280px]">
+                <QuizProvider quiz={quiz}/>
+            </div>
         </div>
+
     );
 }
