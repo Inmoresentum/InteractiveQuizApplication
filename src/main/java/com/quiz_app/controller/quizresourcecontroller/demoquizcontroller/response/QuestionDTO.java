36a3f10f4a2f8350713c6b7f94377626d586097c
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-07-11 00:15:06 +0600

    fix: fixed the json serialization issue
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- src/main/java/com/quiz_app/controller/quizresourcecontroller/demoquizcontroller/response/QuestionDTO.java
+++ src/main/java/com/quiz_app/controller/quizresourcecontroller/demoquizcontroller/response/QuestionDTO.java
@@ -1,6 +1,7 @@
 package com.quiz_app.controller.quizresourcecontroller.demoquizcontroller.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.*;
 
 import java.util.List;
@@ -19,6 +20,7 @@ public class QuestionDTO {
     private String questionPic;
     private String answerSelectionType;
     private List<String> answers;
+    @JsonSerialize(using = CorrectAnswerSerializer.class)
     private List<Integer> correctAnswer;
     private String messageForCorrectAnswer;
     private String messageForIncorrectAnswer;
