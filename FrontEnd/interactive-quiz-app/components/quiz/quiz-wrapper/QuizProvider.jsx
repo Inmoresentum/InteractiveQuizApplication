ccfaf52347fd3f312b9896a0346cf56c8e639248
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-07-11 00:40:10 +0600

    test: testing QuizProvider features
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- FrontEnd/interactive-quiz-app/components/quiz/quiz-wrapper/QuizProvider.jsx
+++ FrontEnd/interactive-quiz-app/components/quiz/quiz-wrapper/QuizProvider.jsx
@@ -5,6 +5,7 @@ import {useState} from "react";
 
 export default function QuizProvider({quiz}) {
     const [quizResult, setQuizResult] = useState()
+    console.log("I am trying to provide some data ")
     console.log(quiz)
     return (
         <Quiz
@@ -14,9 +15,9 @@ export default function QuizProvider({quiz}) {
             // continueTillCorrect
             onComplete={setQuizResult}
             onQuestionSubmit={(obj) => console.log("user question results:", obj)}
-            disableSynopsis
+            disableSynopsis={false}
             // revealAnswerOnSubmit
-            // allowNavigation
+            allowNavigation
         />
     );
 }
