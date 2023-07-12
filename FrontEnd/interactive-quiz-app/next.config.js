0a1162b099b20ed0c1a297ceb04a5f1ca5a9c7ba
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-07-12 22:30:07 +0600

    chore(config): updated next.config.js to allow images data from back-end
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- FrontEnd/interactive-quiz-app/next.config.js
+++ FrontEnd/interactive-quiz-app/next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true
     },
+    images:{
+        remotePatterns: [
+            {
+                protocol: "http",
+                hostname: "localhost",
+                port: 8080,
+                pathname: "/**"
+            }
+        ]
+    }
 }
 
 module.exports = nextConfig
