12f11a815d5d6f8f05d5da1cb388dd2e9cd29857
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-07-16 18:43:27 +0600

    chore(next.config): temporarily removed remote image config
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- FrontEnd/interactive-quiz-app/next.config.js
+++ FrontEnd/interactive-quiz-app/next.config.js
@@ -3,16 +3,6 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true
     },
-    images:{
-        remotePatterns: [
-            {
-                protocol: "http",
-                hostname: "localhost",
-                port: 8080,
-                pathname: "/**"
-            }
-        ]
-    }
 }
 
 module.exports = nextConfig
