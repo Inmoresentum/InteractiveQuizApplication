975bd52d0705bf592cfbce2dc1abe7f35eb66b0b
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-07-22 06:07:23 +0600

    docs: updated OpenAPI documentation for ReportController
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- src/main/java/com/quiz_app/controller/reportcontroller/ReportController.java
+++ src/main/java/com/quiz_app/controller/reportcontroller/ReportController.java
@@ -1,6 +1,8 @@
 package com.quiz_app.controller.reportcontroller;
 
 import com.quiz_app.service.report.ReportService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,11 +19,55 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReportController {
     private final ReportService reportService;
 
+    @Operation(
+            summary = "Get endpoint for get user report in CSV format",
+            description = "By hitting this end point authorized persons will be able" +
+                    " to generate a CSV report of all users." +
+                    " This process is expensive and can " +
+                    "take a lot of time depending number" +
+                    " of users the platform have.",
+            responses = {
+                    @ApiResponse(
+                            description = "If Successfully Generated CSV File",
+                            responseCode = "201"
+                    ),
+                    @ApiResponse(
+                            description = "If any server error",
+                            responseCode = "503"
+                    ),
+                    @ApiResponse(
+                            description = "Unauthorized / Invalid Access Token",
+                            responseCode = "403"
+                    )
+            }
+    )
     @GetMapping(value = "/users/csv", produces = MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity<?> generateUserReportCSV() {
          return reportService.generateUserReportCSV();
     }
 
+    @Operation(
+            summary = "Get endpoint for get user report in PDF format",
+            description = "By hitting this end point authorized persons will be able" +
+                    " to generate a PDF report of all users." +
+                    " This process is expensive and can " +
+                    "take a lot of time depending number" +
+                    " of users the platform have.",
+            responses = {
+                    @ApiResponse(
+                            description = "If Successfully Generated PDF File",
+                            responseCode = "201"
+                    ),
+                    @ApiResponse(
+                            description = "If any server error",
+                            responseCode = "503"
+                    ),
+                    @ApiResponse(
+                            description = "Unauthorized / Invalid Access Token",
+                            responseCode = "403"
+                    )
+            }
+    )
     @GetMapping(value = "/users/pdf", produces = MediaType.APPLICATION_PDF_VALUE)
     public ResponseEntity<?> generateUserReportPDF() {
         return reportService.generateUserReportPDF();
