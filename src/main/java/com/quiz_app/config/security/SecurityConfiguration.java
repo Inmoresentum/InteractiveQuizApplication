ccd1003f2bede3df000a801aee0c4e291f471261
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-07-22 07:06:56 +0600

    security(config): Compliance for Spring Security 7
    :lock: updated security config to comply with the latest version of spring and spring security as older ways have been deprecated and after updated 7 they will be removed.
    
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- src/main/java/com/quiz_app/config/security/SecurityConfiguration.java
+++ src/main/java/com/quiz_app/config/security/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -32,55 +33,60 @@ public class SecurityConfiguration {
     private final LogoutHandler logoutHandler;
 
     @Bean
-    @SuppressWarnings("removal")
-    // Starting from Spring Security 7, the below configuration will not work.
+    // Updated the security config to the latest recommendation implementation
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         // for auto generated api documentation
         http
-                .csrf()
-                .disable()
-                .authorizeHttpRequests()
-                .requestMatchers(
-                        "/api/v1/auth/**",
-                        "/api/v1/quiz/public/**",
-                        "/api/v1/storage/public/**",
-                        "/api/v1/reports/users/csv",
-                        "/api/v1/reports/users/pdf",
-                        "/v2/api-docs",
-                        "/v3/api-docs",
-                        "/v3/api-docs/**",
-                        "/swagger-resources",
-                        "/swagger-resources/**",
-                        "/configuration/ui",
-                        "/configuration/security",
-                        "/swagger-ui/**",
-                        "/webjars/**",
-                        "/swagger-ui.html"
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        auth -> auth.requestMatchers(
+                                "/api/v1/auth/**",
+                                "/api/v1/quiz/public/**",
+                                "/api/v1/storage/public/**",
+                                "/api/v1/reports/users/csv",
+                                "/api/v1/reports/users/pdf",
+                                "/v2/api-docs",
+                                "/v3/api-docs",
+                                "/v3/api-docs/**",
+                                "/swagger-resources",
+                                "/swagger-resources/**",
+                                "/configuration/ui",
+                                "/configuration/security",
+                                "/swagger-ui/**",
+                                "/webjars/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
+                        .requestMatchers("/api/v1/management/**")
+                                .hasAnyRole(Role.ADMIN.name(),
+                                        Role.USER.name())
+                        .requestMatchers(GET, "/api/v1/management/**")
+                                .hasAnyAuthority(Permission.ADMIN_READ.name(),
+                                        Role.USER.name())
+                        .requestMatchers(POST, "/api/v1/management/**")
+                                .hasAnyAuthority(Permission.ADMIN_CREATE.name(),
+                                        Role.USER.name())
+                        .requestMatchers(PUT, "/api/v1/management/**")
+                                .hasAnyAuthority(Permission.ADMIN_UPDATE.name(),
+                                        Role.USER.name())
+                        .requestMatchers(DELETE, "/api/v1/management/**")
+                                .hasAnyAuthority(Permission.ADMIN_DELETE.name(),
+                                        Role.USER.name())
+                        .anyRequest()
+                        .authenticated()
                 )
-                .permitAll()
-
-
-                .requestMatchers("/api/v1/management/**").hasAnyRole(Role.ADMIN.name(), Role.USER.name())
-
-
-                .requestMatchers(GET, "/api/v1/management/**").hasAnyAuthority(Permission.ADMIN_READ.name(), Role.USER.name())
-                .requestMatchers(POST, "/api/v1/management/**").hasAnyAuthority(Permission.ADMIN_CREATE.name(), Role.USER.name())
-                .requestMatchers(PUT, "/api/v1/management/**").hasAnyAuthority(Permission.ADMIN_UPDATE.name(), Role.USER.name())
-                .requestMatchers(DELETE, "/api/v1/management/**").hasAnyAuthority(Permission.ADMIN_DELETE.name(), Role.USER.name())
-
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+                .sessionManagement(session
+                        -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                .logout()
-                .logoutUrl("/api/v1/auth/logout")
-                .addLogoutHandler(logoutHandler)
-                .logoutSuccessHandler((request, response, authentication)
-                        -> SecurityContextHolder.clearContext());
+                .logout(configurer -> {
+                    configurer.logoutUrl("/api/v1/auth/logout");
+                    configurer.addLogoutHandler(logoutHandler);
+                    configurer.logoutSuccessHandler(
+                            ((request, response, authentication)
+                                    -> SecurityContextHolder
+                                    .clearContext()));
+                });
 
         return http.build();
     }
