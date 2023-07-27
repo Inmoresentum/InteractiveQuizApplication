0b8ea7d3d5bfbc8e7d65313491e30fcd6531140d
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-07-27 14:16:49 +0600

    chore: added static metadata
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- FrontEnd/interactive-quiz-app/app/auth/register/page.jsx
+++ FrontEnd/interactive-quiz-app/app/auth/register/page.jsx
@@ -3,6 +3,11 @@ import {options} from "@/app/api/auth/[...nextauth]/options";
 import {redirect} from "next/navigation";
 import ClientSideRegiFrom from "@/components/auth/registration/ClinetSideRegistration";
 
+export const metadata = {
+    title: "Register",
+    description: "There is where you can open an account with us" +
+        " and enjoy the benefits of having an account with us",
+}
 export default async function Register() {
     const session = await getServerSession(options);
     if (session) {
