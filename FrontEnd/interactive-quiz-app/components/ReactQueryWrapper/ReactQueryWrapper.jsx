7dabea4715e0712435dbc157da64a354bb4e8c26
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-07-27 14:19:34 +0600

    stale: will try to figure out the issue and fix it
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

---
+++ FrontEnd/interactive-quiz-app/components/ReactQueryWrapper/ReactQueryWrapper.jsx
@@ -0,0 +1,13 @@
+"use client"
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+export default function ReactQueryWrapper({children}) {
+    const queryClient = new QueryClient();
+    return (
+        <>
+            <QueryClientProvider client={queryClient}>
+                {children}
+            </QueryClientProvider>
+        </>
+    );
+}
