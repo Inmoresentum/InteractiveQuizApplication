702d5f4d125fc07c04bdef25b50dad7ef4448c4f
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-07-29 16:01:44 +0600

    feat: added privacy and policy page
    Not just added privacy and policy page which basically a copy past of Terms of Services page.
    
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

---
+++ FrontEnd/interactive-quiz-app/app/privacyandpolicy/page.jsx
@@ -0,0 +1,14 @@
+import PrivacyAndPolicyWrapper from "@/components/PrivacyAndPolicyWrapper/PrivacyAndPolicyWrapper";
+
+export const metadata = {
+    title: "Privacy And Policy",
+    description: "Here we discuss our privacy and policy in details",
+}
+
+export default function PrivacyAndPolicy() {
+    return (
+        <>
+            <PrivacyAndPolicyWrapper/>
+        </>
+    );
+}
