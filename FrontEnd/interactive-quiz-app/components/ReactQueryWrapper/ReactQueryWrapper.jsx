3c8ab94eebd9648eb4d0795f18f72c4577a88adc
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-08-01 10:42:16 +0600

    refactor: refactoring ReactQueryWrapper.jsx
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- FrontEnd/interactive-quiz-app/components/ReactQueryWrapper/ReactQueryWrapper.jsx
+++ FrontEnd/interactive-quiz-app/components/ReactQueryWrapper/ReactQueryWrapper.jsx
@@ -1,8 +1,8 @@
 "use client"
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 export default function ReactQueryWrapper({children}) {
-    const queryClient = new QueryClient();
     return (
         <>
             <QueryClientProvider client={queryClient}>
