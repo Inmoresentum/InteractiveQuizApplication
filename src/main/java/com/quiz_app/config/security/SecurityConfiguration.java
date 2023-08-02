eb678312e3ae22bf0dd86f97f1721024f90678ad
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-08-03 03:30:59 +0600

    fix: fixed the cor issue with spring security
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- src/main/java/com/quiz_app/config/security/SecurityConfiguration.java
+++ src/main/java/com/quiz_app/config/security/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,6 +17,12 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.springframework.http.HttpMethod.DELETE;
 import static org.springframework.http.HttpMethod.GET;
@@ -37,42 +44,43 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         // for auto generated api documentation
         http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         auth -> auth.requestMatchers(
-                                "/api/v1/auth/**",
-                                "/api/v1/quiz/public/**",
-                                "/api/v1/storage/public/**",
-                                "/api/v1/reports/users/csv",
-                                "/api/v1/reports/users/pdf",
-                                "/v2/api-docs",
-                                "/v3/api-docs",
-                                "/v3/api-docs/**",
-                                "/swagger-resources",
-                                "/swagger-resources/**",
-                                "/configuration/ui",
-                                "/configuration/security",
-                                "/swagger-ui/**",
-                                "/webjars/**",
-                                "/swagger-ui.html"
-                        ).permitAll()
-                        .requestMatchers("/api/v1/management/**")
+                                        "/api/v1/auth/**",
+                                        "/api/v1/quiz/public/**",
+                                        "/api/v1/storage/public/**",
+                                        "/api/v1/reports/users/csv",
+                                        "/api/v1/reports/users/pdf",
+                                        "/v2/api-docs",
+                                        "/v3/api-docs",
+                                        "/v3/api-docs/**",
+                                        "/swagger-resources",
+                                        "/swagger-resources/**",
+                                        "/configuration/ui",
+                                        "/configuration/security",
+                                        "/swagger-ui/**",
+                                        "/webjars/**",
+                                        "/swagger-ui.html"
+                                ).permitAll()
+                                .requestMatchers("/api/v1/management/**")
                                 .hasAnyRole(Role.ADMIN.name(),
                                         Role.USER.name())
-                        .requestMatchers(GET, "/api/v1/management/**")
+                                .requestMatchers(GET, "/api/v1/management/**")
                                 .hasAnyAuthority(Permission.ADMIN_READ.name(),
                                         Role.USER.name())
-                        .requestMatchers(POST, "/api/v1/management/**")
+                                .requestMatchers(POST, "/api/v1/management/**")
                                 .hasAnyAuthority(Permission.ADMIN_CREATE.name(),
                                         Role.USER.name())
-                        .requestMatchers(PUT, "/api/v1/management/**")
+                                .requestMatchers(PUT, "/api/v1/management/**")
                                 .hasAnyAuthority(Permission.ADMIN_UPDATE.name(),
                                         Role.USER.name())
-                        .requestMatchers(DELETE, "/api/v1/management/**")
+                                .requestMatchers(DELETE, "/api/v1/management/**")
                                 .hasAnyAuthority(Permission.ADMIN_DELETE.name(),
                                         Role.USER.name())
-                        .anyRequest()
-                        .authenticated()
+                                .anyRequest()
+                                .authenticated()
                 )
                 .sessionManagement(session
                         -> session
@@ -90,4 +98,15 @@ public class SecurityConfiguration {
 
         return http.build();
     }
+
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+//        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
+//        configuration.setAllowedHeaders(List.of("Authorization"));
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
 }
