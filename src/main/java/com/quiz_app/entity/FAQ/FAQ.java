121fd7e68e662b0191c4da4c827476699ba86daa
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-08-04 04:54:39 +0600

    chore(FAQ): added builder pattern
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- src/main/java/com/quiz_app/entity/FAQ/FAQ.java
+++ src/main/java/com/quiz_app/entity/FAQ/FAQ.java
@@ -1,14 +1,15 @@
 package com.quiz_app.entity.FAQ;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Entity
 @Getter
 @Setter
 @ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class FAQ {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,7 +17,7 @@ public class FAQ {
     private Long id;
     @Column(name = "FAQ_question", nullable = false)
     private String question;
-    @Column(name = "FAQ_anwers", nullable = false)
+    @Column(name = "FAQ_anwers", nullable = false, columnDefinition = "TEXT")
     private String answers;
     @Version
     private Integer version;
