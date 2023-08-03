240f6038f8de7f80d969fec24274a2f8767db457
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-08-03 22:29:07 +0600

    fix: fixed pagination issue and grid flashing issue
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- FrontEnd/interactive-quiz-app/components/Dashboard/Admin/allusers/DataTable.jsx
+++ FrontEnd/interactive-quiz-app/components/Dashboard/Admin/allusers/DataTable.jsx
@@ -3,7 +3,6 @@ import {DataGrid, GridToolbar} from "@mui/x-data-grid";
 import {Avatar} from "@mui/material";
 import {BsCheck2All} from "react-icons/bs";
 import {ImCross} from "react-icons/im";
-import {useSession} from "next-auth/react";
 import {useState} from "react";
 import {useQuery} from "@tanstack/react-query";
 import axios from "axios";
@@ -97,10 +96,13 @@ const fetchUsers = async (page, accessToken, refreshToken) => {
 };
 
 export default function DataTable({authInfo}) {
-    const [page, setPage] = useState(0);
+    const [paginationModel, setPaginationModel] = useState({
+        page: 0,
+        pageSize: 100,
+    });
     const {data, isLoading, isError, error} = useQuery(
-        ["users", page],
-        () => fetchUsers(page, authInfo.access_token, authInfo.refresh_token),
+        ["users", paginationModel],
+        () => fetchUsers(paginationModel.page, authInfo.access_token, authInfo.refresh_token),
     );
 
     if (isLoading) return <div>Loading...</div>;
@@ -110,30 +112,31 @@ export default function DataTable({authInfo}) {
 
     return (
         <>
-            <div className="max-w-[1280px] grid">
+            <div className="max-w-[1280px] flex">
                 <div className="p-16">
                     <h1 className="uppercase font-bold text-red-400 text-3xl p-2 flex items-center justify-center">
                         All Users
                     </h1>
-                    <DataGrid className=""
-                              rows={data.content} //here you will have to make changes
-                              columns={columns}
-                              pageSizeOptions={[100]}
-                              checkboxSelection={true}
-                              disableRowSelectionOnClick={true}
-                              pagination={true}
-                              rowCount={data.totalElements}
-                              loading={isLoading}
-                              paginationMode="server"
-                              rowsPerPageOptions={[10, 30, 50, 70, 100]}
-                              onPaginationModelChange={newPage => setPage(newPage.page)}
-                              slots={{toolbar: GridToolbar}}
-                              slotProps={{
-                                  toolbar: {
-                                      showQuickFilter: true,
-                                      quickFilterProps: {debounceMs: 500},
-                                  },
-                              }}
+                    <DataGrid
+                        rows={data.content} //here you will have to make changes
+                        columns={columns}
+                        pageSizeOptions={[100]}
+                        checkboxSelection={true}
+                        disableRowSelectionOnClick={true}
+                        pagination={true}
+                        rowCount={data.totalElements}
+                        loading={isLoading}
+                        paginationModel={paginationModel}
+                        paginationMode="server"
+                        rowsPerPageOptions={[100]}
+                        onPaginationModelChange={setPaginationModel}
+                        slots={{toolbar: GridToolbar}}
+                        slotProps={{
+                            toolbar: {
+                                showQuickFilter: true,
+                                quickFilterProps: {debounceMs: 500},
+                            },
+                        }}
                     />
                 </div>
             </div>
