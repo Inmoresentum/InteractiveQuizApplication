226fe94b53bfd580fdbb74b1a23a7fcd48558f3b
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-08-05 04:14:32 +0600

    chore: added client side hook to fetch faqs from the backend for the lading page
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

---
+++ FrontEnd/interactive-quiz-app/components/hooks/FetchFrequentFAQs.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import {useQuery} from "@tanstack/react-query";
+
+const fetchFAQs = async () => {
+    const response = await axios.get("http://localhost:8080/api/v1/faq/common");
+    console.log("Printing result form FAQ query");
+    console.log(response.data);
+    return response.data;
+}
+
+export default function useFetchAllCommonFAQs() {
+    return useQuery({
+        queryKey: ["common-faq"],
+        queryFn: () => fetchFAQs()
+    });
+}
