5febd2f7e57e06c62d98eea173a0ebc09a76ea83
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-08-05 22:21:48 +0600

    revert: reverted back to the previous version
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- FrontEnd/interactive-quiz-app/app/dashboard/admin/allusers/page.jsx
+++ FrontEnd/interactive-quiz-app/app/dashboard/admin/allusers/page.jsx
@@ -1,11 +1,12 @@
 import DataTable from "@/components/Dashboard/Admin/allusers/DataTable";
 import {getServerSession} from "next-auth";
 import {options} from "@/app/api/auth/[...nextauth]/options";
+import AllUserLoadingSpinner from "@/components/loading-animation/AllUserLoadingSpinner";
 
 export default async function AllUsers() {
     const session = await getServerSession(options);
     console.log(session);
-    return(
+    return (
         <DataTable authInfo={session.user}/>
     );
 }
