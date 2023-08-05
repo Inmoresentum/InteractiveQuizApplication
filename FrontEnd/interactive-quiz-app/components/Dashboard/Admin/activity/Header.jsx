e84fc6c1fd1f2c7a1de3d49331543c4a03f92c26
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-08-05 22:30:04 +0600

    refactor: getting rid of the unnecessary print statements
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- FrontEnd/interactive-quiz-app/components/Dashboard/Admin/activity/Header.jsx
+++ FrontEnd/interactive-quiz-app/components/Dashboard/Admin/activity/Header.jsx
@@ -4,23 +4,21 @@ import NextLog from "../../../../public/next.svg"
 import Link from "next/link";
 
 export default function Header({authInfo}) {
-      console.log(authInfo)
-
     return (
-        <div className='flex justify-between px-4 pt-4 items-center bg-gray-200'>
+        <div className="flex justify-between px-4 pt-4 items-center bg-gray-200">
             <div className="flex flex-col md:flex-row items-center p-2">
                 <Link href={"/"}>
-                <Image src={AppLog} alt={NextLog} className="w-[45px] rounded-[25px] m-2"/>
+                    <Image src={AppLog} alt={NextLog} className="w-[45px] rounded-[25px] m-2"/>
                 </Link>
-            <h2 className="text-2xl">Dashboard</h2>
+                <h2 className="text-2xl">Dashboard</h2>
             </div>
             {/*I will have to change it later*/}
             <div className="font-bold">
-            <div className="flex flex-col md:flex-row items-center">Welcome Back
-                <div className="text-rose-500 ml-2">
-                    {authInfo.user.username}
+                <div className="flex flex-col md:flex-row items-center">Welcome Back
+                    <div className="text-rose-500 ml-2">
+                        {authInfo.user.username}
+                    </div>
                 </div>
-            </div>
             </div>
         </div>
     );
