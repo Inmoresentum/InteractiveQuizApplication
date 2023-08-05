068f761f49611a0b7ff2290c8771f792a3af0686
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-08-06 03:20:29 +0600

    feat: activity page now fetches the latest data from backend using JWT
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- FrontEnd/interactive-quiz-app/app/dashboard/admin/activity/page.js
+++ FrontEnd/interactive-quiz-app/app/dashboard/admin/activity/page.js
@@ -12,7 +12,7 @@ export default async function AdminDashBoard() {
         <>
             <main className="bg-gray-100 min-h-screen">
                 <Header authInfo={session}/>
-                <TopCard/>
+                <TopCard authInfo={session}/>
                 <div className="p-4 grid md:grid-cols-3 grid-cols-1 gap-4">
                     <BarChar/>
                     <RecentlyAddedQuizzes/>
--- FrontEnd/interactive-quiz-app/components/Dashboard/Admin/activity/Topcard.jsx
+++ FrontEnd/interactive-quiz-app/components/Dashboard/Admin/activity/Topcard.jsx
@@ -1,5 +1,11 @@
-export default function TopCard() {
-    // I will make them dynamic later
+"use client"
+import useFetchDailySystemStat from "@/components/hooks/FetchDailySystemStat";
+
+export default function TopCard({authInfo}) {
+    console.log(authInfo)
+    const response = useFetchDailySystemStat(authInfo.user.access_token, authInfo.user.refresh_token);
+    if (response.isLoading) return;
+    if (response.isError) return <div>{response.isError}</div>;
     return (
         // Add data dynamically by doing an api call using REACT query
         // While data is loading show a loading spinner
@@ -7,35 +13,35 @@ export default function TopCard() {
             <div className="lg:col-span-2 col-span-1 bg-white flex justify-between w-full border  drop-shadow-xl shadow-lg shadow-blue-300
             p-4 rounded-lg hover:shadow-2xl duration-300 ease-in">
                 <div className="flex flex-col w-full pb-4">
-                    <p className="text-2xl font-bold">7,846</p>
+                    <p className="text-2xl font-bold">{response.data.totalNumberOfUsers}</p>
                     <p className="text-gray-600">Total Users</p>
                 </div>
                 {/*Based on increase and decrease, show appropriate styles */}
-                <p className='bg-green-200 flex justify-center items-center p-2 rounded-lg md:hover:scale-110
-                md:hover:cursor-pointer duration-300 ease-linear'>
-                    <span className="text-green-700 text-lg">+18%</span>
+                <p className="bg-green-200 flex justify-center items-center p-2 rounded-lg md:hover:scale-110
+                md:hover:cursor-pointer duration-300 ease-linear">
+                    <span className="text-green-700 text-lg">100%</span>
                 </p>
             </div>
             <div className="lg:col-span-2 col-span-1 bg-white flex justify-between w-full border  drop-shadow-xl shadow-lg shadow-green-500
             p-4 rounded-lg hover:shadow-2xl duration-300 ease-in">
                 <div className="flex flex-col w-full pb-4">
-                    <p className="text-2xl font-bold">10001</p>
-                    <p className='text-gray-600'>Total Quizzes</p>
+                    <p className="text-2xl font-bold">{response.data.totalNumberOfQuizzes}</p>
+                    <p className="text-gray-600">Total Quizzes</p>
                 </div>
-                <p className='bg-green-200 flex justify-center items-center p-2 rounded-lg md:hover:scale-110
-                md:hover:cursor-pointer duration-300 ease-linear'>
-                    <span className='text-green-700 text-lg'>+11%</span>
+                <p className="bg-green-200 flex justify-center items-center p-2 rounded-lg md:hover:scale-110
+                md:hover:cursor-pointer duration-300 ease-linear">
+                    <span className="text-green-700 text-lg">+100%</span>
                 </p>
             </div>
-            <div className='bg-white flex justify-between w-full border p-4 rounded-lg drop-shadow-xl shadow-lg shadow-rose-200
-            hover:shadow-2xl duration-300 ease-in'>
-                <div className='flex flex-col w-full pb-4'>
-                    <p className='text-2xl font-bold'>11,437</p>
-                    <p className='text-gray-600'>Quiz Played Today</p>
+            <div className="bg-white flex justify-between w-full border p-4 rounded-lg drop-shadow-xl shadow-lg shadow-rose-200
+            hover:shadow-2xl duration-300 ease-in">
+                <div className="flex flex-col w-full pb-4">
+                    <p className="text-2xl font-bold">{response.data.totalNumberOfQuizzesPlayedToday}</p>
+                    <p className="text-gray-600">Quiz Played Today</p>
                 </div>
-                <p className='bg-green-200 flex justify-center items-center p-2 rounded-lg md:hover:scale-110
-                md:hover:cursor-pointer duration-300 ease-linear'>
-                    <span className='text-green-700 text-lg'>+17%</span>
+                <p className="bg-green-200 flex justify-center items-center p-2 rounded-lg md:hover:scale-110
+                md:hover:cursor-pointer duration-300 ease-linear">
+                    <span className="text-green-700 text-lg">0%</span>
                 </p>
             </div>
         </div>
