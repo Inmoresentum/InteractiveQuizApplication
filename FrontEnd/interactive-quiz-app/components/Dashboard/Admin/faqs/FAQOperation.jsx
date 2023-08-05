970689a39fe9a25d0a303c29cc898e7b410e6d41
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-08-05 21:38:14 +0600

    fix: made FAQOperation.jsx mobile responsive
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- FrontEnd/interactive-quiz-app/components/Dashboard/Admin/faqs/FAQOperation.jsx
+++ FrontEnd/interactive-quiz-app/components/Dashboard/Admin/faqs/FAQOperation.jsx
@@ -4,10 +4,10 @@ import {Button} from "@/components/ui/button";
 
 export default function FAQActions() {
     return (
-        <div className="flex justify-between w-full rounded-3xl p-4 mb-4
+        <div className="flex flex-col  items-center md:flex-row md:justify-between w-full rounded-3xl p-4 mb-4
         bg-gradient-to-r from-gray-200 via-gray-400 to-gray-600 shadow-xl">
             <div>
-                <Button className="hover:bg-green-600 duration-300 ease-in font-semibold rounded-3xl h-12">
+                <Button className="hover:bg-green-600 duration-300 mb-2 md:mb-0 ease-in font-semibold rounded-3xl h-12">
                     Add new FAQ
                     <SiAddthis className="ml-2 hover:text-black" size={24}/>
                 </Button>
