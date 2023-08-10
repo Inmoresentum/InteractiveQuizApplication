243907d9558d65a406fee928c64dbabab559fe10
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-08-10 06:58:49 +0600

    feat: added service to scan files for viruses
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

---
+++ src/main/java/com/quiz_app/service/clamav/ClamAVService.java
@@ -0,0 +1,32 @@
+package com.quiz_app.service.clamav;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+@Log4j2
+public class ClamAVService {
+    private final WebClient webClient;
+
+    public ClamAVService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl("http://localhost:7075").build();
+    }
+
+    public boolean scanFile(byte[] fileBytes) {
+        try {
+            String result = webClient.post()
+                    .uri("/scan")
+                    .body(BodyInserters.fromValue(fileBytes))
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+
+            return result != null && result.contains("OK");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
