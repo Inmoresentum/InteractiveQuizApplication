54cec7010d6435dc60a50af920422c0537448884
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-08-11 19:46:31 +0600

    feat: added SEO for quiz create page
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- FrontEnd/interactive-quiz-app/app/quiz/create/page.jsx
+++ FrontEnd/interactive-quiz-app/app/quiz/create/page.jsx
@@ -3,6 +3,12 @@ import {options} from "@/app/api/auth/[...nextauth]/options";
 import {getServerSession} from "next-auth";
 import {redirect} from "next/navigation";
 
+
+export const metadata = {
+    title: "Quiz Creation Form",
+    description: "This where users can create quizzes of various kinds"
+}
+
 export default async function QuizCreationPage() {
     const session = await getServerSession(options);
     // if (!session) {
