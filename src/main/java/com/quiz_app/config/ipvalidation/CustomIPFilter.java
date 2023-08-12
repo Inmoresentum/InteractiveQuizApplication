614fab9e6b8dab316fc221ab1aa01b1424898952
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-08-13 04:20:24 +0600

    attention: updated IP filter to incorporate these changes
    If things go wrong, then revert back
    
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- src/main/java/com/quiz_app/config/ipvalidation/CustomIPFilter.java
+++ src/main/java/com/quiz_app/config/ipvalidation/CustomIPFilter.java
@@ -1,10 +1,12 @@
 package com.quiz_app.config.ipvalidation;
 
+import com.quiz_app.service.ipservice.IPService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.web.util.matcher.IpAddressMatcher;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -12,23 +14,20 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class CustomIPFilter extends OncePerRequestFilter {
+    private final IPService ipService;
+
     @Override
     protected void doFilterInternal(@NotNull HttpServletRequest request,
                                     @NotNull HttpServletResponse response,
                                     @NotNull FilterChain filterChain)
             throws ServletException, IOException {
-//        System.out.println("custom docker ip :" + request.getRemoteAddr());
-        if (matches(request, "127.0.0.1")
-                || matches(request, "0:0:0:0:0:0:0:1")
-                || matches(request, "172.20.0.1/8")
-        ) {
+        String remoteAddr = request.getRemoteAddr();
+        if (ipService.isBanned(remoteAddr)) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied");
+        } else {
             filterChain.doFilter(request, response);
         }
-    }
-
-    private boolean matches(HttpServletRequest request, String subnet) {
-        IpAddressMatcher ipAddressMatcher = new IpAddressMatcher(subnet);
-        return ipAddressMatcher.matches(request);
     }
 }
