fe607332a676209e2ae975dcef4247d3b83ea19e
abuudarda <abu.darda@g.bracu.ac.bd>
2023-08-13 06:13:05 +0600

    feat: used bearer tokens to check if admin is downloading reports

--- FrontEnd/interactive-quiz-app/components/Dashboard/Admin/allusers/DataTable.jsx
+++ FrontEnd/interactive-quiz-app/components/Dashboard/Admin/allusers/DataTable.jsx
@@ -10,6 +10,8 @@ import {Button} from "@/components/ui/button";
 import {Label} from "@/components/ui/label";
 import {Input} from "@/components/ui/input";
 import AllUserLoadingSpinner from "@/components/loading-animation/AllUserLoadingSpinner";
+// import {useDownloadReports} from "@/components/hooks/useDownloadReports";
+import axios from "axios";
 
 const columns = [
     {field: "id", headerName: "ID", width: 70},
@@ -86,14 +88,103 @@ export default function DataTable({authInfo}) {
     const handleClose = () => {
         setOpen(false);
     };
+    const downloadPDF = async (accessToken, refreshToken) => {
+        try {
+            const response = await axios.get("http://localhost:8080/api/v1/reports/users/pdf", {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+                responseType: 'blob', // This tells Axios to treat the response as binary data
+            });
 
-    const downloadPDF = () => {
-        window.open("http://localhost:8080/api/v1/reports/users/pdf");
+            // Handle the response and download the file...
+            const blob = new Blob([response.data], { type: 'application/pdf' });
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = 'report.pdf'; // Set the desired file name
+            a.click();
+            URL.revokeObjectURL(url);
+
+
+        } catch (error) {
+            if (error.response.status === 403) {
+                // Access token has expired, try refreshing it
+                try {
+                    const refreshResponse = await axios.post("http://localhost:8080/api/v1/auth/refresh-token", null, {
+                        headers: {
+                            Authorization: `Bearer ${refreshToken}`,
+                        },
+                    });
+
+                    const newAccessToken = refreshResponse.data.access_token;
+                    await downloadPDF(newAccessToken, refreshToken); // Retry with new token
+                } catch (refreshError) {
+                    console.error("Error refreshing token:", refreshError);
+                }
+            } else {
+                console.error("Error downloading PDF:", error);
+            }
+        }
     };
 
-    const downloadCSV = () => {
-        window.open("http://localhost:8080/api/v1/reports/users/csv");
+    const downloadCSV = async (accessToken, refreshToken) => {
+        try {
+            const response = await axios.get("http://localhost:8080/api/v1/reports/users/csv", {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+                responseType: 'blob', // This tells Axios to treat the response as binary data
+            });
+
+            // Handle the response and download the file...
+            const blob = new Blob([response.data], { type: 'text/csv' });
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = 'report.csv'; // Set the desired file name
+            a.click();
+            URL.revokeObjectURL(url);
+
+
+        } catch (error) {
+            if (error.response.status === 403) {
+                // Access token has expired, try refreshing it
+                try {
+                    const refreshResponse = await axios.post("http://localhost:8080/api/v1/auth/refresh-token", null, {
+                        headers: {
+                            Authorization: `Bearer ${refreshToken}`,
+                        },
+                    });
+
+                    const newAccessToken = refreshResponse.data.access_token;
+                    await downloadCSV(newAccessToken, refreshToken); // Retry with new token
+                } catch (refreshError) {
+                    console.error("Error refreshing token:", refreshError);
+                }
+            } else {
+                console.error("Error downloading CSV:", error);
+            }
+        }
     };
+    // const downloadReportsQuery = useDownloadReports();
+    // const downloadPDF = () => {
+    //     // window.open("http://localhost:8080/api/v1/reports/users/pdf");
+    //     downloadReportsQuery.refetch({
+    //         endpoint: "http://localhost:8080/api/v1/reports/users/pdf",
+    //         accessToken: authInfo.access_token,
+    //         refreshToken: authInfo.refresh_token
+    //     });
+    // };
+    //
+    // const downloadCSV = () => {
+    //     // window.open("http://localhost:8080/api/v1/reports/users/csv");
+    //     downloadReportsQuery.refetch({
+    //         endpoint: "http://localhost:8080/api/v1/reports/users/csv",
+    //         accessToken: authInfo.access_token,
+    //         refreshToken: authInfo.refresh_token
+    //     });
+    // };
 
     return (
         <>
@@ -107,16 +198,26 @@ export default function DataTable({authInfo}) {
                         </h1>
                         <div className="flex items-center mt-4 justify-end space-x-4">
                             <Button
-                                onClick={downloadPDF}
+                                onClick={() => downloadPDF(authInfo.access_token, authInfo.refresh_token)}
                                 variant="outlined"
-                                style={{ backgroundColor: "#007bff", color: "#fff", borderRadius: "8px", marginLeft: "24px",}}
+                                style={{
+                                    backgroundColor: "#007bff",
+                                    color: "#fff",
+                                    borderRadius: "8px",
+                                    marginLeft: "24px",
+                                }}
                             >
                                 Download PDF
                             </Button>
                             <Button
-                                onClick={downloadCSV}
+                                onClick={() => downloadCSV(authInfo.access_token, authInfo.refresh_token)}
                                 variant="outlined"
-                                style={{ backgroundColor: "#007bff", color: "#fff", borderRadius: "8px", marginLeft: "8px", }}
+                                style={{
+                                    backgroundColor: "#007bff",
+                                    color: "#fff",
+                                    borderRadius: "8px",
+                                    marginLeft: "8px",
+                                }}
                             >
                                 Download CSV
                             </Button>
