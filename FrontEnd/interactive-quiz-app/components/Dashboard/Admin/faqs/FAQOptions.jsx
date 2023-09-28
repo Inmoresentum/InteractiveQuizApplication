7924496e65cdd368287a45a92e6366cc978d9c64
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-09-28 13:53:45 +0600

    fix: UX improvement
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- FrontEnd/interactive-quiz-app/components/Dashboard/Admin/faqs/FAQOptions.jsx
+++ FrontEnd/interactive-quiz-app/components/Dashboard/Admin/faqs/FAQOptions.jsx
@@ -1,14 +1,14 @@
 import {Button} from "@/components/ui/button";
-import {FaRegEdit} from "react-icons/fa";
 import {MdDelete} from "react-icons/md";
 import {FAQEdit} from "@/components/Dashboard/Admin/faqs/faqedit";
+import FaqDeleteOperation from "@/components/Dashboard/Admin/faqs/faqDelete";
 
 export default function FAQOptions({question, answer}) {
     return (
         <div className="flex w-full p-1 mb-1">
             <FAQEdit question={question} answer={answer}/>
             <Button type="submit" className="rounded-3xl hover:bg-orange-600 duration-300 shadow-2xl shadow-white ease-in">
-                <MdDelete className="hover:text-black" size={24}/>
+                <FaqDeleteOperation/>
             </Button>
         </div>
     );
---
+++ FrontEnd/interactive-quiz-app/components/Dashboard/Admin/faqs/faqDelete.jsx
@@ -0,0 +1,34 @@
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import {MdDelete} from "react-icons/md";
+
+export default function FaqDeleteOperation() {
+    return (
+        <AlertDialog>
+            <AlertDialogTrigger>
+                <MdDelete className="hover:text-black" size={24}/>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle className="text-2xl text-center text-red-400">Are you absolutely sure you want to delete this FAQ?</AlertDialogTitle>
+                    <AlertDialogDescription className="bg-gray-100 rounded-2xl p-2 text-black font-medium">
+                        This action cannot be undone. This will permanently delete The FAQ.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel className="hover:bg-gray-300 hover:rounded-full ease-linear duration-300">Cancel</AlertDialogCancel>
+                    <AlertDialogAction className="hover:bg-red-500 hover:text-black hover:rounded-full ease-linear duration-300">Continue</AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    );
+}
