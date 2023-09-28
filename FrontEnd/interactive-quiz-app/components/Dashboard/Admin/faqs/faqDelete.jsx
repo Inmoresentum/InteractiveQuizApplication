fb47ce8b6b833d50c070ceab2d7484f0d0aea976
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-09-28 14:18:06 +0600

    chore: minor refactor
    Signed-off-by: Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>

--- FrontEnd/interactive-quiz-app/components/Dashboard/Admin/faqs/faqDelete.jsx
+++ FrontEnd/interactive-quiz-app/components/Dashboard/Admin/faqs/faqDelete.jsx
@@ -19,14 +19,20 @@ export default function FaqDeleteOperation() {
             </AlertDialogTrigger>
             <AlertDialogContent>
                 <AlertDialogHeader>
-                    <AlertDialogTitle className="text-2xl text-center text-red-400">Are you absolutely sure you want to delete this FAQ?</AlertDialogTitle>
+                    <AlertDialogTitle className="text-2xl text-center text-red-400">
+                        Are you absolutely sure you want to delete this FAQ?
+                    </AlertDialogTitle>
                     <AlertDialogDescription className="bg-gray-100 rounded-2xl p-2 text-black font-medium">
                         This action cannot be undone. This will permanently delete The FAQ.
                     </AlertDialogDescription>
                 </AlertDialogHeader>
                 <AlertDialogFooter>
-                    <AlertDialogCancel className="hover:bg-gray-300 hover:rounded-full ease-linear duration-300">Cancel</AlertDialogCancel>
-                    <AlertDialogAction className="hover:bg-red-500 hover:text-black hover:rounded-full ease-linear duration-300">Continue</AlertDialogAction>
+                    <AlertDialogCancel className="hover:bg-gray-300 hover:rounded-full ease-linear duration-300">
+                        Cancel
+                    </AlertDialogCancel>
+                    <AlertDialogAction className="hover:bg-red-500 hover:text-black hover:rounded-full ease-linear duration-300">
+                        Continue
+                    </AlertDialogAction>
                 </AlertDialogFooter>
             </AlertDialogContent>
         </AlertDialog>
